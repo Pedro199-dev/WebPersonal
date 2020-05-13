972c0d5da1761278527db427746accca5ade8620
Pedro <hernansaldana52@gmail.com>
2020-05-12 21:16:24 -0500

    El proyecto ya cuenta con vistas desde plantillas

--- core/views.py
+++ core/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="about">Acerca de</a></li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-    <h2>Portada</h2>
-    <p>Esto es la portada</p>
-    """)
+    return render (request, "core/home.html")
 
 
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</h2>
-    <p>Me llamo Pedro y soy programador.</p>
-    """)
+    return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+
+
+def contact(request):
+    return render(request, "core/contact.html")

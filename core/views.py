6ce60a4ccab73d60527dd7dbb5c797456ebb6efa
Pedro <hernansaldana52@gmail.com>
2020-05-12 21:49:19 -0500

    El proyecto ya cuenta con url dinamica y herencia de estructuras

--- core/views.py
+++ core/views.py
@@ -4,13 +4,11 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render (request, "core/home.html")
 
-
 def about(request):
     return render(request, "core/about.html")
 
 def portfolio(request):
     return render(request, "core/portfolio.html")
-
 
 def contact(request):
     return render(request, "core/contact.html")

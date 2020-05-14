9253006696f2061ef8eca5a732c03d6c67a9b254
Pedro <hernansaldana52@gmail.com>
2020-05-14 18:46:16 -0500

    Proyecto terminado

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Direccion Web", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
 
